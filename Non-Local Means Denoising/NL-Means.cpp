fa24797770b3442e4f9858e7f4b33be92eeab5a8
Dylanin1999 <Dylanin1999in1999@163.com>
2020-02-13 23:57:34 +0800

    添加注释和修改bug

--- Non-Local Means Denoising/NL-Means.cpp
+++ Non-Local Means Denoising/NL-Means.cpp
@@ -2,24 +2,28 @@
 #include <opencv.hpp>
 #include <string>
 
+#define CompZero(dis, sigma) (((dis-(2*sigma*sigma))<0)?0:(dis-(2*sigma*sigma)))
 
-#define MIN2(a, b) ((a) < (b) ? (a) : (b))
-#define MAX2(a, b) ((a) > (b) ? (a) : (b))
-#define CLIP3(x, a, b) MIN2(MAX2(a,x), b)
-
-void NL_Means(unsigned char* srcImg, int Height,int Width, int D, int d,int h)
+void NL_Means(unsigned char* srcImg, int Height,int Width, int D, int d,int sigma)
 {
-	unsigned char* afterProcess = (unsigned char*)malloc(sizeof(unsigned char)* Height*Width);
-	int ImgSize = Height*Width;
-	int SearchZoneSize = 2*D+1;
-	int BlockSize = 2 * d + 1;
-	int sumCP = 0;
-	int pixelMinus = 0;
-	int distances = 0;
-	int DisWeight = 0;
-	int HH = h*h;
-	int pixelSum = 0;
-	int Up = 0;
+	if (D<=d)
+	{
+		std::cout << "Please check your params!"<<std::endl;
+	}
+	std::cout << "start to process!" << std::endl;
+	unsigned char* CopyImg = (unsigned char*)malloc(sizeof(unsigned char)* Height*Width);
+	memcpy(CopyImg, srcImg, sizeof(unsigned char)* Height*Width);
+	float ImgSize = Height*Width;
+	float SearchZoneSize = 2*D+1;
+	float BlockSize = 2 * d + 1;
+	float sumCP = 0;
+	float pixelMinus = 0;
+	float distances = 0;
+	float DisWeight = 0;
+	float h = 0.4*sigma;
+	float HH = h*h;
+	float pixelSum = 0;
+	float Up = 0;
 	for (int i{ 0 };i<Height-D;i++)
 	{
 		if (i<D)
@@ -28,25 +32,31 @@ void NL_Means(unsigned char* srcImg, int Height,int Width, int D, int d,int h)
 		{
 			if (j<D)
 				continue;
-			for (int block_i = i-D-d;block_i<i+D-d;block_i++)
+			//单个像素的search zone求值
+			for (int block_i = i-D+d;block_i<i+D-d;block_i++) //blcok的中心纵坐标j的在search zone中的变化范围
 			{
-				for (int block_j = j - D -d; block_j < j + D - d; block_j++)
+				for (int block_j = j - D + d; block_j < j + D - d; block_j++) //blcok的中心横坐标i的在search zone中的变化范围
 				{
-					for (int zone_i = 0;zone_i<2 * d + 1;zone_i++)
+					//单个block求值
+					for (int zone_i = -d;zone_i<=d;zone_i++) 
 					{
-						for (int zone_j = 0; zone_j < 2 * d + 1; zone_j++)
+						for (int zone_j = -d; zone_j <=d; zone_j++)
 						{
-							pixelMinus += pow(srcImg[(block_i + zone_i)*(block_j + zone_j)] - srcImg[i*j],2); //单个zone的所有像素差值相加
+							pixelMinus += pow(CopyImg[(block_i + zone_i)*(block_j + zone_j)] - CopyImg[i*j],2); //单个zone的所有像素差值相加
 						}
 					}
-					distances = pixelMinus/(BlockSize*BlockSize);
-					DisWeight = exp(-(distances / HH));
+					distances = pixelMinus/(BlockSize*BlockSize); //将block内的像素差求平均
+					pixelMinus = 0;
+					DisWeight = exp(-(CompZero(distances,sigma) / HH));//与block的欧氏距离
+					sumCP += DisWeight; //将search zone求得的所有权重求和
+					pixelSum += CopyImg[i*j] * DisWeight; //将像素值和权值相乘   Σui(q)*w(p,q)
 				}
-				sumCP += DisWeight;
-				pixelSum += srcImg[i*j] * DisWeight;
+				std::cout << "processing : " << double(i*j) / (Height*Width) * 100 << "%" << std::endl;
 			}
 			Up = pixelSum / sumCP;
-			afterProcess[i*j] = Up;
+			srcImg[i*j] = Up;
+			Up = 0;
+			sumCP = 0;
 		}
 	}
 }
@@ -58,7 +68,8 @@ int main()
 	cv::Mat gray;
 	cv::cvtColor(pic, gray, CV_BGR2GRAY);
 	cv::imshow("before", gray);
-	NL_Means(gray.data, gray.cols, gray.rows, 10, 5, 2);
+	std::cout << "cols: " << gray.cols << "rows: " << gray.rows << std::endl;
+	NL_Means(gray.data, gray.cols, gray.rows, 10, 1,10);
 	cv::imshow("after", gray);
 	cv::waitKey(0);
 	return 0;
