59d5b1378b4b6c8a40c38f7dbd225a6e4916d076
Dylanin1999 <dylanin1999@163.com>
2020-02-18 00:15:07 +0800

    添加了注释，增加了多线程处理，提高速度

--- Non-Local Means Denoising/NL-Means.cpp
+++ Non-Local Means Denoising/NL-Means.cpp
@@ -4,7 +4,6 @@
 #include "ProgressBar.h"
 #include <thread>
 
-#define CompZero(dis, sigma) (((dis-(2*sigma*sigma))<0)?0:(dis-(2*sigma*sigma)))
 #define MIN2(a, b) ((a) < (b) ? (a) : (b))
 #define MAX2(a, b) ((a) > (b) ? (a) : (b))
 #define Compare(x, a, b) MIN2(MAX2(a,x), b)
@@ -60,85 +59,38 @@ void NL_Means(unsigned char* srcImg, int Height,int Width, int D, int d,int h)
 					int certern_y = Compare((j + SearchZone_j), 0, Width - 1);
 					pixelSum += CopyImg[certern_x*Width+ certern_y] * DisWeight; //将像素值和权值相乘   Σui(q)*w(p,q)
 				}
-				//double progress = double(i*j) / (Height*Width) * 100;
 			}
-			Up = pixelSum / sumCP;
-			Up = Compare(Up, 0, 255);
-			srcImg[i*Width+j] = Up;
-
+			
+			if (sumCP!=0)
+			{
+				Up = pixelSum / sumCP;
+				Up = Compare(Up, 0, 255);
+				srcImg[i*Width + j] = (int)Up;
+			}
 		}
 	}
 }
 
-//void NLM(unsigned char* srcData, int width, int height, int D, int d, float h)
-//{
-//	unsigned char* tempData = (unsigned char*)malloc(sizeof(unsigned char) * width * height);
-//	memcpy(tempData, srcData, sizeof(unsigned char) * height * width);
-//	float sw = 0;
-//	float sum = 0;
-//	int px, py, cx, cy;
-//	float zx;
-//	float vxsy = 0;
-//	float DD = d * d;
-//	float HH = h * h;
-//	for (int j = 0; j < height; j++)
-//	{
-//		for (int i = 0; i < width; i++)
-//		{
-//			sw = 0;
-//			zx = 0;
-//			sum = 0;
-//			for (int n = -D; n <= D; n++)
-//			{
-//				for (int m = -D; m <= D; m++)
-//				{
-//					vxsy = 0;
-//					for (int kn = -d; kn <= d; kn++)
-//					{
-//						for (int km = -d; km <= d; km++)
-//						{
-//							cx = CLIP3(i - d + km, 0, width - 1);
-//							cy = CLIP3(j - d + kn, 0, height - 1);
-//							px = CLIP3(i + m + km, 0, width - 1);
-//							py = CLIP3(j + n + kn, 0, height - 1);
-//							vxsy += (tempData[px + py * width] - tempData[cx + cy * width]) * (tempData[px + py * width] - tempData[cx + cy * width]);
-//						}
-//					}
-//					vxsy = vxsy / DD;
-//					sw = exp(-vxsy / HH);
-//					zx += sw;
-//					int ox = CLIP3(i + m, 0, width - 1);
-//					int oy = CLIP3(j + n, 0, height - 1);
-//					sum += sw * tempData[ox + oy * width];
-//				}
-//			}
-//			srcData[i + j * width] = zx == 0 ? srcData[i + j * width] : CLIP3(sum / zx, 0, 255);
-//		}
-//	}
-//	free(tempData);
-//};
-
 int main()
 {
 	
-	cv::Mat pic = cv::imread("2.jpg");
+	cv::Mat pic = cv::imread("InputImg.jpg"); //输入图像
 	cv::Mat outImg;
-	//cv::cvtColor(pic, gray, CV_BGR2GRAY);
 	std::vector<cv::Mat> colorSp;
 	cv::split(pic, colorSp);
 	int Height = pic.rows;
 	int Width = pic.cols;
-	std::thread t1(NL_Means, colorSp[0].data, Height, Width, 10, 3, 10);
+	std::thread t1(NL_Means, colorSp[0].data, Height, Width, 10, 3, 10);//三通道多线程
 	std::thread t2(NL_Means, colorSp[1].data, Height, Width, 10, 3, 10);
 	std::thread t3(NL_Means, colorSp[2].data, Height, Width, 10, 3, 10);
 	
 	t1.join();
 	t2.join();
 	t3.join();
-	cv::merge(colorSp, outImg);
-	cv::imshow("after", outImg);
+	cv::merge(colorSp, outImg);//通道合并
+	cv::imshow("after", outImg);//输出图像
 	std::cout << "done" << std::endl;
-	cv::imwrite("123567.jpg", outImg);
+	cv::imwrite("outImg.jpg", outImg); //保存输出图像
 	cv::waitKey(0);
 	return 0;
 }
