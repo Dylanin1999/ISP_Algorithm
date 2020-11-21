51b0e29748d42c60bef87a6d121a3ac02c802604
dylan <dylanin1999@163.com>
2020-11-21 23:06:31 +0800

    fix some bug

--- FAST_CornerDetection/FAST.cpp
+++ FAST_CornerDetection/FAST.cpp
@@ -1,22 +1,17 @@
-#include <iostream>
+ï»¿#include <iostream>
 #include <opencv.hpp>
 #include <vector>
 
-
 using namespace cv;
 
-int arr[16][2] = { {-3,-1},{-3,0},{-3,1},
-				   {-2,2},
-				   {-1,3},{0,3},{1,3},
-				   {2,2},
-				   {3,1},{3,0},{3,-1},
-				   {2,-2},
-				   {1,-3},{0,-3},{-1,-3},
-				   {-2,-2}};
-
-
-
-
+int arr[16][2] = { { -3, -1 }, { -3, 0 }, { -3, 1 },
+{ -2, 2 },
+{ -1, 3 }, { 0, 3 }, { 1, 3 },
+{ 2, 2 },
+{ 3, 1 }, { 3, 0 }, { 3, -1 },
+{ 2, -2 },
+{ 1, -3 }, { 0, -3 }, { -1, -3 },
+{ -2, -2 } };
 
 int main()
 {
@@ -35,23 +30,22 @@ int main()
 		uchar* buttomRow = gray.ptr<uchar>(i - radius);
 		uchar* midCol = gray.ptr<uchar>(i);
 		int pixCounter = 0;
-		for (int j = radius; j < width- radius; j++)
+		for (int j = radius; j < width - radius; j++)
 		{
 			uchar center = midCol[j];
 			int counter = 0;
-			//¼ÆËãp1,p9,p5,p13ÓëÖÐÐÄµÄÏñËØ²î
 			counter += topRow[j] - center > threshold ? 0 : 1;
 			counter += buttomRow[j] - center > threshold ? 0 : 1;
 			counter += midCol[j - radius] - center > threshold ? 0 : 1;
-			counter += midCol[j + radius] - center> threshold?0:1;
+			counter += midCol[j + radius] - center> threshold ? 0 : 1;
 
 			if (counter < 3)
 				continue;
-			
-			//Ê¹ÓÃ27µÄÔ­Òò£ºÕâÑù¾Í¿ÉÒÔ½«ÆäÆ´½Ó³ÉÒ»¸öÔ²ÐÎ
+
 			for (int k = 0; k < 27; k++)
 			{
-				uchar pixelValue = gray.ptr<uchar>(i + arr[i][0])[j + arr[i][1]];
+				//std::cout << "K: " << k << std::endl;
+				uchar pixelValue = gray.ptr<uchar>(i + arr[k%16][0])[j + arr[k%16][1]];
 				bool flag = abs(pixelValue - center) > threshold ? true : false;
 				if (flag)
 					pixCounter += 1;
@@ -71,7 +65,15 @@ int main()
 			}
 		}
 	}
+
+	for (int i = 0; i < Corner.size(); i++)
+	{
+		cv::circle(src, Corner[i], 1, cv::Scalar(0, 255, 255));
+	}
 	std::cout << "size: " << Corner.size() << std::endl;
+	cv::imshow("pic", src);
+
+	cv::waitKey(0);
 	return 0;
-	
+
 }
