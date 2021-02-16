bdd4275cd8f204e09638f16d0001e2cd98c394a6
dylanin1999 <dylanin1999@163.com>
2021-02-16 22:43:40 +0800

    add aggratation

--- Block-Matching and 3D filtering(BM3D)/BM3D.cpp
+++ Block-Matching and 3D filtering(BM3D)/BM3D.cpp
@@ -167,6 +167,89 @@ void tran1d(std::vector<std::vector<cv::Mat>>& block,int BlockSize)
 	}
 }
 
+void shrink(std::vector<std::vector<cv::Mat>>& block, float threshold)
+{
+	for (int i=0;i<block.size();i++)
+	{
+		for (int j=0;j<block.size();j++)
+		{
+			for (int rows = 0;rows<block[i][j].rows;rows++)
+			{
+				for (int cols = 0;cols<block[i][j].cols;cols++)
+				{
+					block[i][j].at<float>(rows, cols) = fabs(block[i][j].at<float>(rows, cols)) > threshold ? block[i][j].at<float>(rows, cols) : 0;
+				}
+			}
+		}
+	}
+}
+
+void aggregation(vector<std::vector<cv::Mat>>& block, std::vector<cv::Point2d> blockLUPoint,Mat &numerator, Mat &denominator,
+				 float weight, int patchSize, Mat window)
+{
+	cv::Rect Rect;
+	for (int i = 0;i<block.size();i++)
+	{
+		for (int j =  0;j<block[0].size();j++)
+		{
+			Rect.x = blockLUPoint[i * block[0].size() + j].x;
+			Rect.y = blockLUPoint[i * block[0].size() + j].y;
+			Rect.width = patchSize;
+			Rect.height = patchSize;
+			numerator(Rect) += weight * (block[i][j].mul(window));
+			denominator(Rect) += denominator(Rect) + weight * window;
+		}
+	}
+}
+
+float calculate_weight_hd(vector<std::vector<cv::Mat>>& block, int sigma)
+{
+	int num{ 0 };
+	for (int i = 0; i < block.size(); i++)
+	{
+		for (int j = 0; j < block[0].size(); j++)
+		{
+			for (int row = 0;row<block[i][j].rows;row++)
+			{
+				for (int col = 0;col<block[i][j].cols;col++)
+				{
+					num++;
+				}
+			}
+		}
+	}
+	if (num==0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 1.0 / (sigma * sigma * num);
+	}
+}
+
+
+Mat gen_kaiser(int beta, int length)//How to do this?
+{
+	if ((beta == 2) && (length == 8))
+	{
+		Mat window(length, length, CV_32FC1);
+		Mat kai1(length, 1, CV_32FC1);
+		Mat kai1_T(1, length, CV_32FC1);
+		kai1.at<float>(0, 0) = 0.4387;
+		kai1.at<float>(1, 0) = 0.6813;
+		kai1.at<float>(2, 0) = 0.8768;
+		kai1.at<float>(3, 0) = 0.9858;
+		for (int i = 0; i < 4; i++)
+		{
+			kai1.at<float>(7 - i, 0) = kai1.at<float>(i, 0);
+			kai1_T.at<float>(0, i) = kai1.at<float>(i, 0);
+			kai1_T.at<float>(0, 7 - i) = kai1.at<float>(i, 0);
+		}
+		window = kai1 * kai1_T;
+		return window;
+	}
+}
 
 
 int main()
@@ -177,11 +260,21 @@ int main()
 
 	std::vector<std::vector<::Mat>> blockGroup = GetAllBlock(pic, BlockSize, stride, blockLUPoint);
 	BlockDctTrans(blockGroup);
+	tran1d(blockGroup, BlockSize);
+	shrink(blockGroup, Threshold);
 
+	Mat denominator_hd(pic.size(), CV_32FC1, Scalar::all(0));
+	Mat numerator_hd(pic.size(), CV_32FC1, Scalar::all(0));
+
+	float weight = calculate_weight_hd(blockGroup, sigma);
+	int beta = 2;
+	cv::Mat kaiser = gen_kaiser(beta, BlockSize);
+	aggregation(blockGroup, blockLUPoint, numerator_hd, denominator_hd, weight, BlockSize,kaiser);
 	cv::Mat src = pic.clone();
 	int Height = pic.rows;
 	int Width = pic.cols;
-
+	cv::Mat basic = numerator_hd / denominator_hd;
+	cv::imshow("basic", basic);
 	std::vector<std::vector<BlockMessage>> Block;
 	return 0;
 }
